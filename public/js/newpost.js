1868f96299c880108aa7aea38e3ae5a9d1611607
Frankieramirez72 <frankieramirez7211@gmail.com>
2023-09-24 13:27:06 -0700

    fixed project page

--- public/js/newpost.js
+++ public/js/newpost.js
@@ -1,18 +1,18 @@
-const newPostFormHandler = async (event) => {
+const newProjectFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector("#title").value.trim();
-    const description = document.querySelector("#content").value.trim();
+    const name = document.querySelector("#name").value.trim();
+    const description = document.querySelector("#description").value.trim();
   
-    if (title && content) {
-      const response = await fetch("/api/posts/", {
+    if (name && description) {
+      const response = await fetch("/api/projects/", {
         method: "POST",
         body: JSON.stringify({ name, description }),
         headers: { "Content-Type": "application/json" },
       });
   
       if (response.ok) {
-        document.location.replace("/dashboard");
+        document.location.replace("/myguru");
       } else {
         alert(response.statusText);
       }
@@ -20,5 +20,5 @@ const newPostFormHandler = async (event) => {
   };
   
   document
-    .querySelector(".newpost-form")
-    .addEventListener("submit", newPostFormHandler);
+    .querySelector(".newproject-form")
+    .addEventListener("submit", newProjectFormHandler);
