2a19ba9f960f31977a86dbfdb4c812b4fa8ab7e7
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-09-25 21:24:20 -0700

    / same as /homepage in data

--- public/js/myguru.js
+++ public/js/myguru.js
@@ -1,15 +1,18 @@
-const editBtns = document.querySelectorAll(".edit-btn")
-const delBtns = document.querySelectorAll(".del-btn")
+const editBtns = document.querySelectorAll(".edit-btn");
+const delBtns = document.querySelectorAll(".del-btn");
 
-delBtns.forEach(btn => {
-    btn.addEventListener("click", async (event) => {
-    console.log('button clicked', event.currentTarget.dataset.id);
-    const response = await fetch("/api/projects/" + event.currentTarget.dataset.id, {
+delBtns.forEach((btn) => {
+  btn.addEventListener("click", async (event) => {
+    console.log("button clicked", event.currentTarget.dataset.id);
+    const response = await fetch(
+      "/api/projects/" + event.currentTarget.dataset.id,
+      {
         method: "DELETE",
-        headers: { 'Content-Type': 'application/json' },
-    })
-    if (response.ok){
-        document.location.reload()
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    if (response.ok) {
+      document.location.reload();
     }
- })
- });
+  });
+});
