8cf6a70b192fd9f3f4b6572fd7e4869e98411bf3
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-09-25 21:31:42 -0700

    Removing unused code

--- public/js/editpost.js
+++ public/js/editpost.js
@@ -1,49 +1,29 @@
 const editProjectFormHandler = async (event) => {
-    event.preventDefault();
-    if (event.target.hasAttribute("data-id")) {
-      console.log("edit");
-  
-      const id = event.target.getAttribute("data-id");
-      const name = document.querySelector("#name").value.trim();
-      const description = document.querySelector("#description").value.trim();
-      const due_date = document.querySelector("#date").value.trim();
+  event.preventDefault();
+  if (event.target.hasAttribute("data-id")) {
+    console.log("edit");
 
-      if (name && description && due_date) {
-        const response = await fetch(`/api/projects/${id}`, {
-          method: "PUT",
-          body: JSON.stringify({ name, description }),
-          headers: { "Content-Type": "application/json" },
-        });
-  
-        if (response.ok) {
-          document.location.replace("/myguru");
-        } else {
-          alert(response.statusText);
-        }
-      }
-    }
-  };
-  
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute("data-id")) {
-      const id = event.target.getAttribute("data-id");
-  
-      const response = await fetch(`/api/posts/${id}`, {
-        method: "DELETE",
+    const id = event.target.getAttribute("data-id");
+    const name = document.querySelector("#name").value.trim();
+    const description = document.querySelector("#description").value.trim();
+    const due_date = document.querySelector("#date").value.trim();
+
+    if (name && description && due_date) {
+      const response = await fetch(`/api/projects/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({ name, description }),
+        headers: { "Content-Type": "application/json" },
       });
-  
+
       if (response.ok) {
-        document.location.replace("/dashboard");
+        document.location.replace("/myguru");
       } else {
-        alert("Failed to delete the post");
+        alert(response.statusText);
       }
     }
-  };
-  
-  document
-    .querySelector(".editproject-form")
-    .addEventListener("submit", editProjectFormHandler);
-  
-  document
-    .querySelector(".delete-post-button")
-    .addEventListener("click", delButtonHandler);
+  }
+};
+
+document
+  .querySelector(".editproject-form")
+  .addEventListener("submit", editProjectFormHandler);
