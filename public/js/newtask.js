6c6f64c7d32ade2adbf7606045a7b5d709b3818d
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-09-27 09:47:21 -0700

    styling add to calendar button

--- public/js/newtask.js
+++ public/js/newtask.js
@@ -31,3 +31,26 @@ const newTaskFormHandler = async (event) => {
 document
   .querySelector(".newtask-form")
   .addEventListener("submit", newTaskFormHandler);
+
+var nameValue = document.getElementById("name").value;
+var namePlaceholder = document.getElementById("name").placeholder;
+
+var descriptionValue = document.getElementById("description").value;
+var descriptionPlaceholder = document.getElementById("description").placeholder;
+
+var dateValue = document.getElementById("date").value;
+var datePlaceholder = document.getElementById("date").placeholder;
+
+//onchange
+
+const config = {
+  name: nameValue ? nameValue : namePlaceholder,
+  description: descriptionValue ? descriptionValue : descriptionPlaceholder,
+  startDate: dateValue ? dateValue : datePlaceholder,
+  endDate: dateValue ? dateValue : datePlaceholder,
+  options: ["Apple", "Google", "Outlook.com"],
+};
+const button = document.getElementById("my-default-button");
+if (button) {
+  button.addEventListener("click", () => atcb_action(config, button));
+}
